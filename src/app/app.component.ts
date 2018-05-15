bf62b162d4a3bff1f0a0342fdb7ae8eeeb19920c
Paul Healy <pau.coder@outlook.com>
2018-05-15 04:43:55 +0100

    chore: finetune

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
 
-import { LocaleService, TranslationService, Language } from 'angular-l10n';
 
 @Component({
   selector: 'app-root',
@@ -13,37 +12,15 @@ import { LocaleService, TranslationService, Language } from 'angular-l10n';
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit, OnDestroy {
-  @Language() lang: string;
   title = 'app';
 
-  constructor(
-    public locale: LocaleService,
-    public translationService: TranslationService
-  ) { }
+  constructor() { }
 
   public ngOnInit() {
     console.log('Initializing the app component');
-
-    // this.translationService.translationChanged().subscribe(
-    //   () => {
-    //     console.log('local chnageed!!');
-    //     this.changeDetectorRef.markForCheck();
-    //   }
-    // );
   }
 
   public ngOnDestroy() {
     console.log('Destroying the app component');
-  }
-
-  toggleLanguage() {
-    console.log(33333, this.locale.getCurrentLanguage());
-    if (this.locale.getCurrentLanguage() === 'en') {
-      this.locale.setDefaultLocale('nl', 'NL');
-      this.locale.setCurrentCurrency('EURO');
-    } else if (this.locale.getCurrentLanguage() === 'nl') {
-      this.locale.setDefaultLocale('en', 'US');
-      this.locale.setCurrentCurrency('USD');
-    }
   }
 }
