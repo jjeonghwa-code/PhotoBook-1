465dd5cadd9b122e5945a93b87015ea0b1028a82
Paul Healy <pau.coder@outlook.com>
2018-05-25 10:28:18 +0100

    feat: implemented file upload and download features in step 1

--- src/app/magazine/create/step-1-photos/step-1-photos.component.ts
+++ src/app/magazine/create/step-1-photos/step-1-photos.component.ts
@@ -1,6 +1,6 @@
 import * as _ from 'lodash';
 import { Component, OnInit, Input, Output, EventEmitter, HostListener } from '@angular/core';
-import { FilesService } from '../../../shared/services';
+import { FilesService , AppStateService, CommonService } from '../../../shared/services';
 
 @Component({
   selector: 'app-step-1-photos',
@@ -10,19 +10,28 @@ import { FilesService } from '../../../shared/services';
 export class Step1PhotosComponent implements OnInit {
   errors: Array<string> =[];
   dragAreaClass: string = 'dragarea';
-  @Input() projectId: number = 100;
-  @Input() sectionId: number = 100;
   @Input() fileExt: string[] = ["JPG", "JPEG"];
   @Input() maxFiles: number = 100;
   @Input() maxSize: number = 5; // 5MB
   @Output() uploadStatus = new EventEmitter();
 
-  imgSrcs: any[] = [];
+  files = this.appStateService.files || [];
+  newFiles: any[] = [];
+  imageScaleUrl = this.filesService.imageScaleUrl;
+  timestamp = new Date().getMilliseconds();
+  selectedImage = 0;
+  amountOfPhotosToUpload = 0;
+  isUploading = false;
+  rotation = 0;
   
-  constructor(private fileService: FilesService) { }
+  constructor(
+    private filesService: FilesService,
+    private appStateService: AppStateService,
+    public commonService: CommonService
+  ) { }
 
   ngOnInit() {
-    this.fileService.getFolder()
+    this.filesService.getFolder()
       .subscribe(
         success => {
           this.refreshPhotoList();
@@ -32,63 +41,108 @@ export class Step1PhotosComponent implements OnInit {
       })
   }
 
-  onFileChange(event){
-    let files = event.target.files; 
-    this.saveFiles(files);
-  }
-
-  @HostListener('dragover', ['$event']) onDragOver(event) {
-      this.dragAreaClass = "droparea";
-      event.preventDefault();
-  }
-
-  @HostListener('dragenter', ['$event']) onDragEnter(event) {
-      this.dragAreaClass = "droparea";
-      event.preventDefault();
-  }
-
-  @HostListener('dragend', ['$event']) onDragEnd(event) {
-      this.dragAreaClass = "dragarea";
-      event.preventDefault();
-  }
-
-  @HostListener('dragleave', ['$event']) onDragLeave(event) {
-      this.dragAreaClass = "dragarea";
-      event.preventDefault();
-  }
-  @HostListener('drop', ['$event']) onDrop(event) {   
-      this.dragAreaClass = "dragarea";           
-      event.preventDefault();
-      event.stopPropagation();
-      var files = event.dataTransfer.files;
-      this.saveFiles(files);
-  }
-
   refreshPhotoList(files = []) {
-    if (files.length > 0) {
-      files.forEach((file) => {
-        const myReader: FileReader = new FileReader();
-  
-        myReader.onloadend = (e) => {
-          const base64Data = myReader.result;
-          this.imgSrcs.push(base64Data); 
-        };
-    
-        myReader.readAsDataURL(file);
-      })  
-    } else {
-      this.fileService.getFolderPhotos()
-        .subscribe((res) => {
-          if (parseInt(res.errNum) == 100) {
+    this.filesService.getFolderPhotos()
+      .subscribe((data) => {
+        if (parseInt(data.errNum) == 100) {
+          this.files = [];
+          this.appStateService.files = this.files;
+          this.filesService.files = this.files;
+          return;
+        }
 
+        if (parseInt(data.errNum) != 200) {
+          return;
+        }
+
+        const newFiles = [];
+        data.images.forEach((image, idx) => {
+          const file = image;
+          const currentFile = this.filesService.getFileById(file.imgID);
+          const newFile = {
+            url: file.imgUrl,
+            name: file.imgName,
+            orientation: +file.imgWidth > +file.imgHeight ? 0 : 1,
+            isCover: (file.isCover == true) ? true : false,
+            id: file.imgID,
+            height: +file.imgHeight,
+            width: +file.imgWidth,
+            text: file.text,
+            weight: idx,
+          };
+
+          if (currentFile) {
+            // These properties can be updated by the server, so remove these.
+            delete currentFile.width;
+            delete currentFile.height;
+            delete currentFile.orientation;
+
+            _.merge(newFile, currentFile);
+            newFiles.push(newFile);
+          } else {
+            newFiles.push(newFile);
           }
-        },(err) => {
-            console.log(err);
         })
+
+        // Sort by weight
+        newFiles.sort((a, b) => {
+          var keyA = parseInt(a.weight),
+              keyB = parseInt(b.weight);
+
+          if(keyA < keyB) return -1;
+          if(keyA > keyB) return 1;
+          return 0;
+        });
+
+        newFiles.forEach((newFile, idx) => {
+          newFile.weight = idx;
+        })
+
+        this.files = newFiles;
+        this.appStateService.files = this.files;
+        this.filesService.files = this.files;
+      },(err) => {
+        console.log('Error listing folder contents', err);
+      })
+  }
+
+  setMainImage(selectedIndex) {
+    this.selectedImage = selectedIndex;
+  }
+
+  uploadNextFile() {
+    if (this.newFiles.length > 0) {
+      this.isUploading = true;
+      const file = this.newFiles.shift();
+      this.filesService.uploadFile(file)
+        .subscribe(
+          success => {
+            this.uploadNextFile();
+          },
+          error => {
+            this.uploadNextFile(); // Just skip the file
+        });
+    } else {
+      this.isUploading = false;
+      this.refreshPhotoList();
     }
   }
 
-  saveFiles(files){
+  saveNewFiles(files){
+    this.errors = []; // Clear error
+    // Validate file size and allowed extensions
+    if (files.length > 0 && (!this.isValidFiles(files))) {
+        this.uploadStatus.emit(false);
+        return;
+    }  
+  
+    if (files.length > 0) {
+      this.newFiles = _.values(files);
+      this.uploadNextFile();
+    }
+  }
+
+  saveFilesFromLocal(files){
     this.errors = []; // Clear error
     // Validate file size and allowed extensions
     if (files.length > 0 && (!this.isValidFiles(files))) {
@@ -98,27 +152,30 @@ export class Step1PhotosComponent implements OnInit {
   
     if (files.length > 0) {
       const filesArry = _.values(files);
-      this.refreshPhotoList(filesArry);
+      files.forEach((file) => {
+        const myReader: FileReader = new FileReader();
+  
+        myReader.onloadend = (e) => {
+          const base64Data = myReader.result;
+          // this.imgSrcs.push(base64Data);
 
-      // let formData: FormData = new FormData();
-      // for (var j = 0; j < files.length; j++) {
-      //   formData.append("file[]", files[j], files[j].name);
-      // }
-      // var parameters = {
-      //   projectId: this.projectId,
-      //   sectionId: this.sectionId
-      // }
-      const file = filesArry.shift();
-      this.fileService.uploadFile(file)
-        .subscribe(
-          success => {
-            this.uploadStatus.emit(true);
-            console.log(success)
-          },
-          error => {
-            this.uploadStatus.emit(true);
-            this.errors.push(error.ExceptionMessage);
-        })
+          var newFile = {
+            url: base64Data,
+            name: '',
+            orientation: 0,
+            isCover: false,
+            id: '',
+            height: 0,
+            width: 0,
+            text: '',
+            weight: '',
+          };
+
+          this.files.push(newFile);
+        };
+    
+        myReader.readAsDataURL(file);
+      })        
     }
   }
 
@@ -150,11 +207,42 @@ export class Step1PhotosComponent implements OnInit {
         }
   }
 
-
   private isValidFileSize(file) {
         var fileSizeinMB = file.size / (1024 * 1000);
         var size = Math.round(fileSizeinMB * 100) / 100; // convert upto 2 decimal place
         if (size > this.maxSize)
             this.errors.push("Error (File Size): " + file.name + ": exceed file size limit of " + this.maxSize + "MB ( " + size + "MB )");
+  }
+
+  onFileChange(event){
+    let files = event.target.files; 
+    this.saveNewFiles(files);
+  }
+
+  @HostListener('dragover', ['$event']) onDragOver(event) {
+      this.dragAreaClass = "droparea";
+      event.preventDefault();
+  }
+
+  @HostListener('dragenter', ['$event']) onDragEnter(event) {
+      this.dragAreaClass = "droparea";
+      event.preventDefault();
+  }
+
+  @HostListener('dragend', ['$event']) onDragEnd(event) {
+      this.dragAreaClass = "dragarea";
+      event.preventDefault();
+  }
+
+  @HostListener('dragleave', ['$event']) onDragLeave(event) {
+      this.dragAreaClass = "dragarea";
+      event.preventDefault();
+  }
+  @HostListener('drop', ['$event']) onDrop(event) {   
+      this.dragAreaClass = "dragarea";           
+      event.preventDefault();
+      event.stopPropagation();
+      var files = event.dataTransfer.files;
+      this.saveNewFiles(files);
   }
 }
