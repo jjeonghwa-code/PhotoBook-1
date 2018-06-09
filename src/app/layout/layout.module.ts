76b95d3cf745664c7e232b06dbf31b8f4b004dec
Paul Healy <pau.coder@outlook.com>
2018-06-09 03:30:23 +0100

    fix: imported the router module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { L10nModule } from '@photobook/l10n';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     L10nModule
   ],
   exports: [
