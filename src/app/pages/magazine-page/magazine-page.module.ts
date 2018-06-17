518ba5e3842cd7ea79d909d6ba9ae2d46ce0f58c
Paul Healy <pau.coder@outlook.com>
2018-06-17 20:25:48 +0100

    fix: styled the nav bar

--- src/app/pages/magazine-page/magazine-page.module.ts
+++ src/app/pages/magazine-page/magazine-page.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { L10nModule } from '@photobook/l10n';
 import { MagazinePageRoutingModule } from './magazine-page-routing.module';
 import { MagazinePageComponent } from './magazine-page.component';
 import { StepNavigationComponent } from './components/step-navigation/step-navigation.component';
@@ -8,6 +9,7 @@ import { StepNavigationComponent } from './components/step-navigation/step-navig
 @NgModule({
   imports: [
     CommonModule,
+    L10nModule,
     MagazinePageRoutingModule
   ],
   declarations: [MagazinePageComponent, StepNavigationComponent]
